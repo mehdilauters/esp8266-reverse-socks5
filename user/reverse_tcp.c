951076d9612667115f452ad4ea0a192fafb07c66
Mehdi <mehdilauters@gmail.com>
2017-01-05 14:05:33 +0100

    Working reverse tcp

--- user/reverse_tcp.c
+++ user/reverse_tcp.c
@@ -53,18 +53,17 @@ int create_socket(char *_server, int _port) {
 
 char server_buffer[256];
 char client_buffer[256];
-int sockfd_server;
-int sockfd_client;
+int sockfd_server = -1;
+int sockfd_client = -1;
 
 TaskHandle_t server_client_task = NULL;
 TaskHandle_t client_server_task = NULL;
 bool running;
 
 void reverse_tcp_stop() {
-  close(sockfd_server);
   close(sockfd_client);
   vTaskDelete( client_server_task );
-  vTaskDelete( server_client_task );
+  sockfd_client = -1;
 }
 
 void reverse_tcp_client_to_server(void *pvParameters) {
@@ -72,23 +71,40 @@ void reverse_tcp_client_to_server(void *pvParameters) {
     int res = read(sockfd_client, client_buffer, sizeof(client_buffer));
     if(res > 0) {
       write(sockfd_server, client_buffer, res);
-//       printf("client=>server\n");
       vTaskDelay(100 / portTICK_PERIOD_MS );
     } else {
       printf("client read error\n");
-      running = false;
       vTaskDelay(100);
+      reverse_tcp_stop();
     }
   }
 }
 
 void reverse_tcp_server_to_client(void *pvParameters) {
   while(true) {
+    memset(server_buffer, 0, sizeof(server_buffer));
     int res = read(sockfd_server, server_buffer, sizeof(server_buffer));
     if(res > 0) {
-      write(sockfd_client, server_buffer, res);
+      if(sockfd_client < 0) {
+        char * separator = strstr(server_buffer, ":");
+        if(separator != NULL) {
+          int value_len = separator - server_buffer;
+          memcpy(m_client, server_buffer, value_len);
+          m_client[value_len] = '\0';
+          char *p = server_buffer + value_len + 1; // ":" length
+          m_port_client = strtol(p, NULL, 10);
+          sockfd_client = create_socket(m_client, m_port_client);
+          if(sockfd_client < 0) {
+            printf("Could not connect to client\n");
+          } else {
+            printf("Tunneling %s:%d <=> %s:%d\n", m_server, m_port, m_client, m_port_client);
+            xTaskCreate(reverse_tcp_client_to_server, (const char *)"reverse_tcp_client_to_server", 512, NULL, 1, &client_server_task);
+          }
+        }
+      } else {
+        write(sockfd_client, server_buffer, res);
+      }
       vTaskDelay(100 / portTICK_PERIOD_MS );
-//       printf("server=>client\n");
     } else {
       printf("server read error\n");
       running = false;
@@ -101,43 +117,42 @@ void reverse_tcp_task(void *pvParameters) {
   while(1) {
     running = true;
     sockfd_server = create_socket(m_server, m_port);
-    sockfd_client = create_socket(m_client, m_port_client);
     bool start = true;
     if(sockfd_server < 0) {
       printf("Could not connect to server\n");
-      if(sockfd_client >= 0) {
-        close(sockfd_client); 
-      }
-      start = false;
-    }
-    
-    if(sockfd_client < 0) {
-      printf("Could not connect to server\n");
-      if(sockfd_server >= 0) {
-        close(sockfd_server);
-      }
       start = false;
     }
     
     if(start) {
       printf("starting...\n");
-      xTaskCreate(reverse_tcp_client_to_server, (const char *)"reverse_tcp_client_to_server", 512, NULL, 1, &client_server_task);//1024,866
-      xTaskCreate(reverse_tcp_server_to_client, (const char *)"reverse_tcp_server_to_client", 512, NULL, 1, &server_client_task);//1024,866
+      xTaskCreate(reverse_tcp_server_to_client, (const char *)"reverse_tcp_server_to_client", 512, NULL, 1, &server_client_task);
       while(running) vTaskDelay(100);
       reverse_tcp_stop();
+      vTaskDelete( server_client_task );
+      close(sockfd_server);
     }
+    
+    
+//     sockfd_client = create_socket(m_client, m_port_client);
+//     if(sockfd_client < 0) {
+//       printf("Could not connect to server\n");
+//       if(sockfd_server >= 0) {
+//         close(sockfd_server);
+//       }
+//       start = false;
+//     }
+//     
+//     if(start) {
+//       xTaskCreate(reverse_tcp_client_to_server, (const char *)"reverse_tcp_client_to_server", 512, NULL, 1, &client_server_task);
+//       reverse_tcp_stop();
+//     }
     vTaskDelay(1000   / portTICK_PERIOD_MS);
   }
 }
 
 
-void start_reverse_tcp(const char * _server, int _port, const char * _client, int _client_port) {
+void start_reverse_tcp(const char * _server, int _port) {
   memcpy(m_server, _server, strlen(_server));
   m_port = _port;
-  
-  memcpy(m_client, _client, strlen(_client));
-  m_port_client = _client_port;
-  
-  printf("Tunneling %s:%d <=> %s:%d\n", m_server, m_port, m_client, m_port_client);
-  xTaskCreate(reverse_tcp_task, (const char *)"reverse_tcp_task", 512, NULL, 1, NULL);//1024,866
+  xTaskCreate(reverse_tcp_task, (const char *)"reverse_tcp_task", 512, NULL, 1, NULL);
 }
--- user/reverse_tcp.h
+++ user/reverse_tcp.h
@@ -1,6 +1,6 @@
 #ifndef REVERSE_TCP_H 
 #define REVERSE_TCP_H
 
-void start_reverse_tcp(const char * _ip, int _port, const char * _client, int _client_port);
+void start_reverse_tcp(const char * _ip, int _port);
 
 #endif
